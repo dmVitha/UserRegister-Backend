b16b25218a6c71965eca01ae555987d9b43289f1
dmVitha <dmv1995vitha@gmail.com>
2018-10-06 13:07:35 +0530

    user register backend

---
+++ src/main/java/com/example/SpringBoot/SpringBootApp/SpringBootAppApplication.java
@@ -0,0 +1,20 @@
+package com.example.SpringBoot.SpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/SpringBoot/SpringBootApp/model/User.java
@@ -0,0 +1,4 @@
+package com.example.SpringBoot.SpringBootApp.model;
+
+public class User {
+}
